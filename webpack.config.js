11c16abae6808e9ec8666dcc984da2a5177d7aa5
211016759 <wes.bornor@ge.com>
2016-12-27 16:07:28 -0500

    local dev server build with hot component replacement webpack & babel transpile reference component

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var debug = process.env.NODE_ENV !== "production";
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devServer: !debug ? null : {
+        inline: true,
+        hot: true,
+        port: 3000
+    },
+    devtool: debug ? "source-map" : null,
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "dist/bundle.js",
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel'],
+            exclude: /node_modules/
+        }]
+    },
+    plugins: debug ? [] : [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
+        ]
+};
