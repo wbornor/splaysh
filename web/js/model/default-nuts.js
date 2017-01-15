5556ab3d9fdd8088a90123975d7a49979abed9b0
211016759 <wes.bornor@ge.com>
2017-01-15 13:54:49 -0500

    move default nuts into own library

---
+++ web/js/model/default-nuts.js
@@ -0,0 +1,102 @@
+/**
+ * Created by wesbornor on 1/15/17.
+ */
+
+export const DefaultNuts = {
+    all: {
+        id: 'all',
+        nut_id: 9999,
+        type: 'ALL',
+        title: 'All',
+        thumbnail: 'all.jpg',
+        items: []
+    },
+    talknut: {
+        id: 'talknut',
+        nut_id: 1,
+        type: 'TALKNUT',
+        title: 'Talk',
+        thumbnail: 'talknut.jpg',
+        items: []
+    },
+    favnut: {
+        id: 'favnut',
+        nut_id: 2,
+        type: 'FAVNUT',
+        title: 'Favs',
+        thumbnail: 'favnut.jpg',
+        items: []
+    },
+    photonut: {
+        id: 'photonut',
+        nut_id: 3,
+        type: 'PHOTONUT',
+        title: 'Photos',
+        thumbnail: 'flickr_logo.png',
+        items: []
+    },
+    wishnut: {
+        id: 'wishnut',
+        nut_id: 4,
+        type: 'WISHNUT',
+        title: 'Wishlist',
+        thumbnail: 'wishnut.jpeg',
+        items: []
+    },
+    medianut: {
+        id: 'medianut',
+        nut_id: 6,
+        type: 'MEDIANUT',
+        title: 'Media',
+        thumbnail: 'netflix-icon.png',
+        items: []
+    },
+    projectnut: {
+        id: 'projectnut',
+        nut_id: 7,
+        type: 'PROJECTNUT',
+        title: 'Projects',
+        thumbnail: 'debian.png',
+        items: []
+    },
+    analnut: {
+        id: 'analnut',
+        nut_id: 8,
+        type: 'ANALNUT',
+        title: 'Analogies',
+        thumbnail: 'Thing1thing2.jpg',
+        items: []
+    },
+    vidinut: {
+        id: 'vidinut',
+        nut_id: 9,
+        type: 'VIDINUT',
+        title: 'Videos',
+        thumbnail: 'vidinut.jpeg',
+        items: []
+    },
+    audinut: {
+        id: 'audinut',
+        nut_id: 10,
+        type: 'AUDINUT',
+        title: 'Audio',
+        thumbnail: 'headphones.jpg',
+        items: []
+    },
+    budnut: {
+        id: 'budnut',
+        nut_id: 12,
+        type: 'BUDNUT',
+        title: 'Buds',
+        thumbnail: 'Facebook-Icon.png',
+        items: []
+    },
+    mapnut: {
+        id: 'mapnut',
+        nut_id: 13,
+        type: 'MAPNUT',
+        title: 'Maps',
+        thumbnail: 'mapnut.jpeg',
+        items: []
+    }
+};
